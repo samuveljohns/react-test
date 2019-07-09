5184e31fd10bdf3c2b2ff3ce74f28518f90bb6d4
samuvel <samuvel246@gmail.com>
2019-07-09 19:22:23 +0530

    Add enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <code>src/App.js</code>;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
