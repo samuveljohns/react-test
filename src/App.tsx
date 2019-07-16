cfd5859c16a7ccda1680956ad859d12e883a70eb
samuvel <samuvel246@gmail.com>
2019-07-16 19:29:22 +0530

    make testcase failed

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export const BoilingVerdict: StatelessComponent<BoilingVerdictProps> = (props)=>
   if (props.celsius >= 100) {
     return (<p>The water would boil.</p>);
   }
-  return (<p>The water would not boil.</p>);
+  return (<p>The water will not boil.</p>);
 }
 
 export class TemperatureInput extends React.Component<TemperatureInputProps, {}> {
